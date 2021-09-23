c3404de8a881363c2ed3c063c6456b487108d3a5
Entotam <dozzator@gmail.com>
2021-09-24 03:55:44 +0400

    add chat view logic to show messages and users

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,4 @@
 class Message < ApplicationRecord
-	validates :body, presence: true, length: { minimum: 3, maximum: 50 }
+	validates :body, presence: true, length: { minimum: 3, maximum: 250 }
 	belongs_to :user
 end
