a75e7d4d498609e186c3f2b8aaaf20becd0f2572
Entotam <dozzator@gmail.com>
2021-09-24 07:05:48 +0400

    Add session, change navigation buttons

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -4,4 +4,22 @@ class SessionController < ApplicationController
 		
 	end
 
+	def create
+		user = User.find_by(username: params[:session][:username].downcase)
+		if user && user.authenticate(params[:session][:password])
+			session[:user_id] = user.id
+			flash[:notice] = "You have logged in"
+			redirect_to root_path
+		else
+			flase[:alert] = "Wrong username or password"
+			render 'new'
+		end
+	end
+
+	def destroy
+		session[:user_id] = nil
+		flash[:notice] = "Logged out"
+		redirect_to root_path
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   
   root 'chatroom#index'
   get 'login', to: 'session#new'
-  post 'login', to: 'sessions#create'
-  
+  post 'login', to: 'session#create'
+  delete 'logout', to: 'session#destroy'
+
 end
